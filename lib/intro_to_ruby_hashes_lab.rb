949bd591b0e8744781c1a62756fd470dbd477098
Jamon Dixon <jamonadixon@gmail.com>
2021-01-15 23:23:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,12 +1,12 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  {:1 => "2"}
+  {key: "value"}
+  hash
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  {:location => "New York City",
-  :occupation => "Cashier"}
+  
 end
 
 def update_counting_hash(hash, key)
